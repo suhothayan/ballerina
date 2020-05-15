bd44433ff3aff5ecad8d9c1de54ec6f0a1baab53
Suhothayan <suhothayan@gmail.com>
2020-05-15 15:47:31 +0530

    Add main and service support for language server SyntaxTree modifier
    Support for insert of main and service functions in SyntaxTree

--- compiler/ballerina-parser/src/main/java/io/ballerinalang/compiler/internal/parser/tree/STNode.java
+++ compiler/ballerina-parser/src/main/java/io/ballerinalang/compiler/internal/parser/tree/STNode.java
@@ -145,7 +145,7 @@ public abstract class STNode {
     private int getFirstChildIndex(STNode... children) {
         for (int index = 0; index < children.length; index++) {
             STNode child = children[index];
-            if (SyntaxUtils.isSTNodePresent(child) && child.width != 0) {
+            if (SyntaxUtils.isSTNodePresent(child) && child.widthWithLeadingMinutiae != 0) {
                 return index;
             }
         }
@@ -155,7 +155,7 @@ public abstract class STNode {
     private int getLastChildIndex(STNode... children) {
         for (int index = children.length - 1; index >= 0; index--) {
             STNode child = children[index];
-            if (SyntaxUtils.isSTNodePresent(child) && child.width != 0) {
+            if (SyntaxUtils.isSTNodePresent(child) && child.widthWithLeadingMinutiae != 0) {
                 return index;
             }
         }
